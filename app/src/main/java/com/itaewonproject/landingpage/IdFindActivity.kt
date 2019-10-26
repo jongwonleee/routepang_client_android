5679ca4b4bac7069348314864340214b973bfd61
이 종원 <jong12em@naver.com>
2019-10-26 15:54:16 +0900

    string mock 수정

--- app/src/main/java/com/itaewonproject/landingpage/IdFindActivity.kt
+++ app/src/main/java/com/itaewonproject/landingpage/IdFindActivity.kt
@@ -2,12 +2,18 @@ package com.itaewonproject.landingpage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.itaewonproject.R
 
 class IdFindActivity : AppCompatActivity() {
 
+    private lateinit var buttonBack:ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_id_find)
+
+        buttonBack = findViewById(R.id.button_back) as ImageView
+        buttonBack.setOnClickListener({finish()})
     }
 }
